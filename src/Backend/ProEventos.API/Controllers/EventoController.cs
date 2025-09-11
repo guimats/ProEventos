c92cacd6d778307b7de51482f4c17959496fcb21
Guilherme <guilherme.mat.ds@gmail.com>
2025-09-11 13:55:38 -0300

    seção 3: introdução ao ef core

--- src/Backend/ProEventos.API/Controllers/EventoController.cs
+++ src/Backend/ProEventos.API/Controllers/EventoController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using ProEventos.API.Data;
 using ProEventos.API.Models;
 using System;
 using System.Collections.Generic;
@@ -11,44 +12,21 @@ namespace ProEventos.API.Controllers
     [Route("[controller]")]
     public class EventoController : ControllerBase
     {
+        private readonly DataContext _context;
 
-        public IEnumerable<Evento> _evento = new Evento[]
-        {
-            new Evento()
-            {
-                Id = 1,
-                Tema = "Angular 11 e .NET 5",
-                Local = "Belo Horizonte",
-                Lote = "1º Lote",
-                QtdPessoas = 250,
-                DataEvento = DateTime.Now.AddDays(2).ToString("dd/MM/yyyy"),
-                ImagemUrl = "foto.png"
-            },
-            new Evento()
-            {
-                Id = 2,
-                Tema = "Angular e suas novidades",
-                Local = "Sao Paulo",
-                Lote = "2º Lote",
-                QtdPessoas = 350,
-                DataEvento = DateTime.Now.AddDays(3).ToString("dd/MM/yyyy"),
-                ImagemUrl = "foto1.png"
-            }
-        };
-
-        public EventoController() { }
+        public EventoController(DataContext context) => _context = context;
 
         [HttpGet]
         public IEnumerable<Evento> Get()
         {
-            return _evento;
+            return _context.Eventos;
         }
 
 
         [HttpGet("{id}")]
-        public IEnumerable<Evento> GetById(int id)
+        public Evento GetById(int id)
         {
-            return _evento.Where(e => e.Id == id);
+            return _context.Eventos.FirstOrDefault(e => e.Id == id);
         }
     }
 }
